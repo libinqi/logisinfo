ad09cdd3bd9390bd0011a10c8ed6e19d9bd06731
libinqi <libinqi@outlook.com>
2013-12-01 23:52:29 +0800

    专线信息基础代码提交

--- app/controllers/line.js
+++ app/controllers/line.js
@@ -2,6 +2,11 @@
 var helpers = require('./_helpers');
 var orm = require('orm');
 var moment = require('moment');
+require('../../public/js/core/line_dict');
+var line_type=[{"id":"1","name":"单程"},{"id":"2","name":"往返"}];var line_goods_type=[{"id":"1","name":"不限"},{"id":"2","name":"普通货物"},{"id":"3","name":"大件货物"},{"id":"4","name":"鲜活易腐"},{"id":"5","name":"危险货物"},{"id":"6","name":"贵重货物"},{"id":"7","name":"保温冷藏"},{"id":"8","name":"搬家货物"}];
+var trans_time=[{"id":"1","name":"1天"},{"id":"2","name":"2天"},{"id":"3","name":"3天"},{"id":"4","name":"4天"},{"id":"5","name":"5天"},{"id":"6","name":"5天以上"}];
+var mode_transport=[{"id":"1","name":"公路运输"},{"id":"2","name":"海上运输"},{"id":"3","name":"铁路运输"},{"id":"4","name":"航空运输"},{"id":"5","name":"邮件运输"},{"id":"6","name":"多式联运"},{"id":"7","name":"固定设施运输"},{"id":"8","name":"内河运输"},{"id":"9","name":"其它运输"}];
+var validate_type=[{"id":"1","name":"长期有效"},{"id":"2","name":"1天"},{"id":"3","name":"7天"},{"id":"4","name":"15天"},{"id":"5","name":"1个月"},{"id":"6","name":"半年"},{"id":"7","name":"1年"}];
 
 module.exports = {
     index: function (req, res, next) {
@@ -21,7 +26,7 @@ module.exports = {
             if (_.isNull(val))return "";
             return val;
         });
-        res.render('line/add', {params: line});
+        res.render('line/add', {params: req.params,line: line,line_type:line_type,trans_time:trans_time,mode_transport:mode_transport,validate_type:validate_type});
     },
     create: function (req, res, next) {
         // req.body.createdAt = new Date();
@@ -66,7 +71,7 @@ module.exports = {
             line.expiryDate = moment(line.expiryDate).format('YYYY-MM-DD HH:mm:ss');
             line.createdAt = moment(line.createdAt).format('YYYY-MM-DD HH:mm:ss');
             line.updatedAt = moment(line.updatedAt).format('YYYY-MM-DD HH:mm:ss');
-            res.render('line/edit', {params: req.params, line: line});
+            res.render('line/edit', {params: req.params, line: line,line_type:line_type,trans_time:trans_time,mode_transport:mode_transport,validate_type:validate_type});
         });
     },
     update: function (req, res, next) {
